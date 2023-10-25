4231994059f546ca7b3a730129b48655955c2ef6
Engi Shark <matthew.fernand@uconn.edu>
2023-10-24 22:25:03 -0400

    student register with lambda

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,7 +4,11 @@
     <p>
       Please enter your studentID and the courseID for the desired course. 
     </p>
-
+    <form @submit.prevent="studentLogin" class="studentLogin">
+      <label for="login">Enter Student ID to load classes</label>
+      <input type="text" id="studentid" name="Student" v-model="studentName" />
+      <button type="submit">Load</button>
+    </form>
     <form @submit.prevent="registerCourse" class="registerCourse">
       <label for="registration">Register for Course</label>
       <input type="text" id="courseid" name="Course" v-model="courseName" />
@@ -43,27 +47,63 @@
 
 <script setup>
 import { ref } from "vue";
+const apiName = 'InsertAPINameHere'; //Change to finalized API database name
 
+const studentName = ref("");
 const courseName = ref("");
-
 const ids = ref([]);
 
 // function to run when the create todo form is submitted
+function studentLogin() {
+  const studentToAdd = studentName.value.trim();
+  const path = '../../Lambda_Functions/Registration/course-view-student.py'; 
+  const MyInit = {
+    headers: {},
+    queryStringParameters: {
+      'netID' : studentToAdd,
+    }
+  };
+  API.get(apiName, path, MyInit) 
+    .then((response) => {
+      ids.value.push(response);
+    });
+}
+
 function registerCourse() {
   // sanitize the input by removing the whitespace from the beginning and end of newTodo.value
   const courseToAdd = courseName.value.trim();
-
+  const studentToAdd = studentName.value.trim();
   // if the sanitized input is not an empty string (i.e., an actual todo), add it to the list and reset the form
   if (courseToAdd != "") {
+    const path = '../../Lambda_Functions/Registration/course-register.py'
+    const MyInit = {
+      headers: {},
+      queryStringParameters: {
+        'netID' : studentToAdd,
+        'courseID' : courseToAdd
+      }
+    }
+    API.post(apiName, path, MyInit)
     ids.value.push(courseToAdd);
-    courseName.value = "";
+    courseName.value = "";  
   }
 }
 
 // when a todo's delete button is clicked, the index of that todo is passed to this function
 // Array.splice takes an index in the array and a number of items to delete after that
 function unregisterCourse(index) {
-  ids.value.splice(index, 1);
+  newVals = ids.value.splice(index, 1);
+  const courseToAdd = newVals.value.trim();
+  const studentToAdd = studentName.value.trim();
+  const path = "../..Lambda_Function/Registration/course-drop.py"
+  const MyInit = {
+    headers: {},
+    queryStringParameters: {
+      'netID' : studentToAdd,
+      'class_ID' : courseToAdd
+    }
+  }
+  API.put(apiName, path, MyInit)
 }
 </script>
 
