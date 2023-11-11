fa464a2b14882112fd98d82bbcbfe05873c5c06b
Julian Dominguez <juliandominguez@Julians-MacBook-Air-3.local>
2023-11-11 09:54:23 -0500

    api support

--- src/views/CourseEdit.vue
+++ src/views/CourseEdit.vue
@@ -1,185 +1,199 @@
 <template>
-    <main class="form">
-      <h2>Course Edit</h2>
-      <p>
-        This page allows modifications to be made to courses by Admins/Professors.
-      </p>
-  
-      <form @submit.prevent="editcourse" class="editcourse">
-        <label for="edit">Search Course</label>
-        <input type="text" id="admin_id" name="admin" v-model="admin_name" />
-        <button type="submit">Search</button>
+  <main class="fetch">
+      <h2>Edit a Course</h2>
+      <p>Load Course Information:</p>
+      <form @submit.prevent="FetchCourse" class="FetchCourse">
+          <label for="courseName">Enter course name to load course:</label>
+          <input type="text" id="courseName" v-model="courseName"/>
+          <button type="submit">Load</button>
       </form>
+      <div v-if="isDataAvailable()">
+          <h3>Course Information:</h3>
+          <table>
+              <thead>
+                  <tr>
+                      <th>Class Name</th>
+                      <th>Class ID</th>
+                      <th>Instructor</th>
+                      <th>Room</th>
+                      <th>Meeting Time</th> 
+                      <th>Prerequisites</th>
+                      <th>Search Tags</th>
+                      <th>Max Enrollment</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr v-for="course in displaycourse" :key="course.class_name">
+                      <td>{{ course.class_name }}</td>
+                      <td>{{ course.class_id }}</td>
+                      <td>{{ course.instructor }}</td>
+                      <td>{{ course.room }}</td>
+                      <td>{{ course.meeting_time }}</td>
+                      <td>{{ course.pre_requisites }}</td>
+                      <td>{{ course.search_tags }}</td>
+                      <td>{{ course.max_enrollment }}</td>
+                  </tr>
+              </tbody>
+              <tfoot>
+                  <tr>
+                      <td><!-- No input needed for class_name --></td> 
+                      <td><input type="text" v-model="newCourse.class_id" placeholder="class ID"></td>
+                      <td><input type="text" v-model="newCourse.instructor" placeholder="Instructor"></td>
+                      <td><input type="text" v-model="newCourse.room" placeholder="Room"></td>
+                      <td><input type="text" v-model="newCourse.meeting_time" placeholder="Meeting Time"></td>
+                      <td><input type="text" v-model="newCourse.pre_requisites" placeholder="Prerequisites"></td>
+                      <td><input type="text" v-model="newCourse.search_tags" placeholder="Search Tags"></td>
+                      <td><input type="number" v-model.number="newCourse.max_enrollment" placeholder="Max Enrollment"></td>
+                      <td><button @click="submitCourse">Submit</button></td>
+                  </tr>
+              </tfoot>
+          </table>
+      <p v-if="message_submit"> {{ message_submit }}</p>
+      </div>
+      <p v-if="message_fetch">{{ message_fetch }}</p>
+  </main>
+</template>
 
+<script setup>
+import { ref, computed } from "vue";
 
-  <div>
-    <table class="fetch">
-      <thead>
-        <tr>
-          <th>Edit Class Name</th>
-          <th>Edit Instructor</th>
-          <th>Edit Room</th>
-          <th>Edit Meeting Time</th>
-          <th>Edit Prerequisites</th>
-          <th>Edit Search Tags</th>
-          <th>Edit Max Enrollment</th>
-          <th>Action</th> <!-- added a new column for the save button -->
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(course, index) in displayCourses" :key="course.class_name">
-          <td>
-            <input type="text" v-model="displayCourses[index].class_name" />
-          </td>
-          <td>
-            <input type="text" v-model="displayCourses[index].instructor" />
-          </td>
-          <td>
-            <input type="text" v-model="displayCourses[index].room" />
-          </td>
-          <td>
-            <input type="text" v-model="displayCourses[index].meeting_time" />
-          </td>
-          <td>
-            <input type="text" v-model="displayCourses[index].pre_requisites" />
-          </td>
-          <td>
-            <input type="text" v-model="displayCourses[index].search_tags" />
-          </td>
-          <td>
-            <input type="number" v-model="displayCourses[index].max_enrollment" />
-          </td>
-          <td>
-            <button @click="saveCourse(index)">Save</button> <!-- Save button -->
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+const newCourse = ref({
+class_name: '',
+class_id: '',
+instructor: '',
+room: '',
+meeting_time: '',
+pre_requisites: '',
+search_tags: '',
+max_enrollment: null
+});
 
+const courseName = ref("");
+const rawApiData = ref(null);
+const data = ref([]); 
+const message_fetch = ref("");
+const message_submit = ref("");
 
+function isDataAvailable() {
+const condition = data.value && data.value.length > 0;
+console.log('Is data available:', condition);
+return condition;
+}
 
-  
-      <ul>
-        <!--
-          v-for requires a unique key for every element so that it can efficiently keep track
-          of each list item. it is possible for the user to type the same todo more than once,
-          so the todo itself isn't necessarily unique. the index on its own is of course unique,
-          it represents each unique place in the array. there are unfortunately edge case issues
-          with using the index alone that we don't need to get into, so we combine the todo and
-          the index into a unique key that will work in all situations
-        -->
+function FetchCourse() {
+  const endpointURL = 'https://7lymtbki38.execute-api.us-east-1.amazonaws.com/Stage_1'; 
+  const path = '/classes/details';
 
-        <!-- <li v-for="(todo, index) in todos" :key="todo + index">
-          {{ todo }}
-          <button @click="delete_edit(index)">Delete</button>
-        </li> -->
-
-        <!-- <li v-for="(edit, index) in edits" :key="edit + index">
-          {{ edits }}
-          <button @click="delete_edit(index)">Delete</button>
-        </li> -->
-
-        <li v-if="edits.length > 0">
-          <p>Edit Courses:</p>
-        </li>
-        <li v-if="edits.length > 0">
-        </li>
-        <li v-for="(edit, index) in edits" :key="edit + index">
-          {{ edit }}
-          <button @click="delete_edit(index)">Delete</button>
-        </li>
-  
-        <!--
-          it is a good user experience practice to provide feedback when in an "emtpy state",
-          in this case, where there are no todos to show yet. this informs the user of
-          the current status of the system (working), and doesn't make them wonder if something
-          has gone wrong
-         -->
-        <!-- <li v-if="todos.length === 0"> -->
-        <li v-if="edits.length === 0">
-          <p>No Courses Entered</p>
-        </li>
-      </ul>
-    </main>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue";
-  const newTodo = ref("");
-  const edits = ref([]);
-  function editcourse() {
-    const todoToAdd = newTodo.value.trim();
-    if (todoToAdd !== "") {
-      edits.value.push(todoToAdd);
-      newTodo.value = "";
-    }
-  }
-  // function deleteTodo(index) {
-    function editCourse(index, updatedCourseData) {
-    const courseToUpdate = displayCourses[index];
-
-    const endpointURL = 'https://7lymtbki38.execute-api.us-east-1.amazonaws.com/Stage_1';
-    const path = '/EditCourse'; 
-    const url = `${endpointURL}${path}`;
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        courseId: courseToUpdate.id, // 'id'is property name used to identify the course
-        updatedCourseData,
+  if (courseName.value !== "") { 
+      const url = `${endpointURL}${path}?class_name=${encodeURIComponent(courseName.value)}`;
+      console.log("Constructed URL:", url);
+      fetch(url, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+          }
       })
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.statusCode === 200) {
-        console.log(data.body); // successfully updated 
-      } else {
-        console.error(data.body); // error updating course
-      }
-    })
-    .catch(error => {
-      console.error("Error updating course:", error);
-    });
+      .then(response => response.json())
+      .then(jsonResponse => {
+          rawApiData.value = JSON.stringify(jsonResponse, null, 2); // For debugging purposes
+          if (jsonResponse && jsonResponse.statusCode === 200) {
+              try {
+                  const bodyObj = JSON.parse(jsonResponse.body);
+                  data.value = [bodyObj];
+                  newCourse.value.class_name = bodyObj.class_name;
+                  message_fetch.value = ""
+              } catch (error) {
+                  console.error('Error parsing JSON response:', error);
+                  data.value = []; 
+                  message_fetch.value = `error fetching course: ${ error.message}`;
+              }
+          } else {
+              console.error('Unexpected response format or status code:', jsonResponse);
+              data.value = [];
+              message_fetch.value = `error fetching course: ${JSON.parse(jsonResponse.body)}`; 
+          }
+      })
+      .catch(error => {
+          console.error('There was an error fetching the courses:', error);
+      });
   }
-  </script>
-  
-  <style>
-  .form {
-    padding: 1rem;
+}
+function submitCourse() {
+console.log('Submitting course:', newCourse.value);
+
+const endpointURL = 'https://7lymtbki38.execute-api.us-east-1.amazonaws.com/Stage_1'; 
+const path = '/classes'
+const url = `${endpointURL}${path}`;
+const courseDataToUpdate = { ...newCourse.value };
+
+for (const key in courseDataToUpdate) {
+  if (courseDataToUpdate[key] == '') {
+    courseDataToUpdate[key] = data.value[0][key];
   }
-  .form h2 {
-    font-size: 1.5rem;
-    margin-bottom: 2rem;
-  }
-  .form p {
-    margin-bottom: 1rem;
-  }
-  /* flex layouts allow us to position elements next to each other that would otherwise have been on top of each other */
-  .form ul {
-    display: flex;
-    gap: 1rem;
-    flex-direction: column;
-  }
-  .form li {
-    display: flex;
-    gap: 0.5rem;
-    align-items: center;
-  }
-  /* create some space beneath the create todo form */
-  .form form {
-    margin-bottom: 1rem;
-  }
-  /* set some default styling to buttons and inputs for borders, heights, and padding */
-  .form :is(input, button) {
-    line-height: 2rem;
-    padding-inline: 0.5rem;
-    border-radius: 0.375rem;
-    border: 1px solid #d9d9d9;
-    margin-left: 0.5rem;
-    color: #202020;
-  }
-  </style>
+}
+fetch(url, {
+  method: 'PUT', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(courseDataToUpdate),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+  message_fetch.value = "course sucessfuly updated";
+})
+.catch((error) => {
+  console.error('Error:', error);
+  message_fetch.value = `error updating course: ${error.message}`;
+});
+}
+
+const displaycourse = computed(() => {
+const courses = data.value.map((course) => {
+  return {
+    class_name: course.class_name,
+    class_id: course.class_id,
+    instructor: course.instructor,
+    room: course.room,
+    meeting_time: course.meeting_time,
+    pre_requisites: course.pre_requisites,
+    search_tags: course.search_tags,
+    max_enrollment: course.max_enrollment,
+  };
+});
+console.log(courses); 
+return courses;
+});
+</script>
+
+<style>
+/* add padding around the page */
+.fetch {
+padding: 1rem;
+}
+/* make the heading font larger and add spacing below */
+.fetch h2 {
+font-size: 1.5rem;
+margin-bottom: 2rem;
+}
+/* add spacing below the text */
+.fetch p {
+margin-bottom: 1rem;
+}
+/* add borders to the table and its top row */
+.fetch table,
+.fetch thead {
+border: 1px solid #d9d9d9;
+}
+/* setup the spacing and the text alignment of the table headers and table cells */
+.fetch :is(th, td) {
+text-align: left;
+padding: 0.25rem 0.75rem;
+min-width: 10rem;
+}
+/* make even numbered rows an off-white color to make the table more legible */
+.fetch tr:nth-child(even) {
+background-color: #f9f9f9;
+}
+</style>
