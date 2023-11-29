3f8336b6519c11daf0c92696477b908ab37ca3cb
Engi Shark <matthew.fernand@uconn.edu>
2023-11-29 14:21:48 -0500

    Login Function, still needs role-access

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,24 @@
           <li>
             <RouterLink to="/">Home</RouterLink>
           </li>
-          <li>
+          <li v-if="isStudent || isAdmin">
             <RouterLink to="/register">Register for Course</RouterLink>
           </li>
-          <li>
+          <li v-if="isStudent || isProf || isAdmin">
             <RouterLink to="/course">View Courses</RouterLink>
           </li>
-          <li>
+          <li v-if="isProf || isAdmin">
             <RouterLink to="/edit">Edit Courses</RouterLink>
           </li>
-          <li>
+          <li v-if="isAdmin">
             <RouterLink to="/usermanage">Manage Users</RouterLink>
           </li>
-          <li>
+          <li v-if="isAdmin">
             <RouterLink to="/usersearch">Search for User</RouterLink>
           </li>
         </ul>
+        <button @click="login" v-if="!isAuthenticated">Log In</button>
+        <button @click="logout" v-if="isAuthenticated">Log Out</button>
       </div>
       <div class="content">
         <RouterView />
@@ -31,9 +33,32 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import { RouterView } from "vue-router";
 import AppHeader from "./components/AppHeader.vue";
+import { useAuth0 } from '@auth0/auth0-vue';
+export default{
+  setup() {
+    const auth0 = useAuth0();
+    return {
+      isAuthenticated: auth0.isAuthenticated,
+      isAdmin: auth0.isAdmin,
+      isStudent: auth0.isStudent,
+      isProf: auth0.isProf,
+      netID: auth0.netID,
+      login: () => {
+        auth0.loginWithRedirect();
+      },
+      logout: () => {
+        auth0.logout({ 
+          logoutParams: { 
+            returnTo: window.location.origin 
+          } 
+        });
+      },
+    };
+  }
+};
 </script>
 
 <style>
