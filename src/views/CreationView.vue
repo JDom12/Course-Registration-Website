e06d7440a457b5c6a7bfa31cf9fcee436a7cc33e
Julian Dominguez <juliandominguez@Julians-MacBook-Air-3.local>
2023-12-02 17:11:34 -0500

    integration with api

--- src/views/CreationView.vue
+++ src/views/CreationView.vue
@@ -6,22 +6,19 @@
     </p>
 
     <form @submit.prevent="createTodo" class="create-todo">
-      <label for="todo">New Course</label>
       <input type="text" id="todo" name="todo" v-model="classname" placeholder = "Classname" />
-      <input type = "text" id="instructor" name="instructor" v-model="instructor" placeholder = "Instructor"/>
+      <input type="text" id="class_id" name="class_id" v-model="class_id" placeholder = "Class ID" />
+      <input type = "text" id="instructor" name="instructor" v-model="instructor" placeholder = "Instructor name"/>
       <input type = "text" id = "room" name="room" v-model= "room" placeholder = "Room" />
-      <input type = "text" id = "meeting_time" name = "meeting_time" v-model = "meeting_time" placeholder = "Meeting Time"/>
+      <input type = "text" id = "search_tags" name="search_tags" v-model= "search_tags" placeholder = "Ex. PHAR" />
+      <input type = "text" id = "prerequisite" name="prerequisite" v-model= "prerequisite" placeholder = "Seperate multiple prerequisites with comma and space" />
+      <input type = "text" id = "meeting_time" name = "meeting_time" v-model = "meeting_time" placeholder = "Ex. M / W / F 1:00-2:00"/>
       <input type = "text" id = "max_enrollment" name = "max_enrollment" v-model = "max_enrollment" placeholder = "Max Enrollment" />
       
       <button type="submit">Save</button>
       <!-- add more fields for each variable-->
     </form>
-    <!-- only add string variables for now - will add update course function later-->
-    <form @submit.prevent = "deleteTodo" class = "delete-todo">
-      <label for = "delete"> Delete Course</label>
-      <input type = "text" id = "delete" name = "delete" v-model="classnamed" placeholder = "Classname" />
-      <button type = "submit">Delete</button>
-    </form>
+    <p v-if="apiResponse">{{ apiResponse }}</p> 
   </main>
 </template>
 
@@ -31,19 +28,20 @@ import { ref } from "vue";
 const classname = ref("") ;  
 const classnamed = ref("");
 
+const class_id = ref("");
 const instructor = ref("") ; 
 const room = ref("");
 const meeting_time = ref("");
-const pre_requisites = ref([]); // list
-const search_tags = ref([]); // list
-const max_enrollment = ref("");
+const prerequisite = ref("");
+const search_tags = ref(""); // list
+const max_enrollment = ref(null);
 
 const apiResponse = ref("");
 
 // function to run when the create todo form is submitted
 function createTodo() {
-  const endpointURL = "https://7lymtbki38.execute-api.us-east-1.amazonaws.com/Stage_1'" ;
-  const path = '/all_classes'; 
+  const endpointURL = "https://7lymtbki38.execute-api.us-east-1.amazonaws.com/Stage_1" ;
+  const path = '/classes'; 
 
   const url = `${endpointURL}${path}`;
 
@@ -53,66 +51,32 @@ function createTodo() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        class_name: classname,
+        'class_name': classname.value,
+        'class_id' : class_id.value,
         'instructor' : instructor.value,
         'room' : room.value,
         'meeting_time' : meeting_time.value,
-        'pre_requisites' : pre_requisites.value,
+        'pre_requisites' : prerequisite.value,
         'search_tags' : search_tags.value,
         'max_enrollment' : max_enrollment.value,
-
       })
     })
     .then(response => response.json())
     .then(data => {
-      apiResponse.value = data.body;
+    apiResponse.value = data.body;
+      // Reset fields after submission
       classname.value = "";
+      class_id.value = "";
       instructor.value = "";
       room.value = "";
       meeting_time.value = "";
-      pre_requisites.value = [];
-      search_tags.value = [];
-      max_enrollment.value = "";
-      // do for all variables
+      prerequisite.value = ""; 
+      search_tags.value = ""; 
+      max_enrollment.value = null;
     })
     .catch(error => {
-      // Handle errors
       console.error("Error creating course:", error);
-    });
-  }
-
-// when a todo's delete button is clicked, the index of that todo is passed to this function
-// Array.splice takes an index in the array and a number of items to delete after that
-function deleteTodo(index) {
-  const coursetoremove =  classnamed.value.trim();
-
-  const endpointURL = "https://7lymtbki38.execute-api.us-east-1.amazonaws.com/Stage_1" ;
-  const path = '/all_classes'; 
-
-  const url = `${endpointURL}${path}`;
-
-  fetch(url, {
-    method: 'DELETE', 
-    headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        class_name : coursetoremove,
-      }
-      )  
-  })
-  .then(response => response.json())
-  .then(data=> {
-    if (data.statusCode === 200) {
-      console.log(data.body); // successfully deleted course
-    } else{
-      console.error(data.body); // error
-    }
-    classnamed.value = "" ;
-  })
-  .catch(error => {
-    console.error("Error deleting course:", error);
-  })
+  });
 }
 
 </script>
